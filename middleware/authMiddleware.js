66acce35d354a1c41acd3fadff2bba42fea0b073
Gabriel <mariano.gabriel.persano@gmail.com>
2024-07-02 18:19:59 -0300

    Detalles en MIddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,14 +4,14 @@ const config = require('../config/config')
 module.exports = (req, res, next) => {
     const authHeader = req.headers['authorization']
 
-    if(!authHeader) return res.status(404).send({auth: false, message: 'No token'})
+    if(!authHeader) return res.status(404).send({auth: false, message: 'No hay token.'})
 
     const token = authHeader.split(' ')[1]
 
-    if(!token) return res.status(404).send({auth: false, message: 'Malformed token'})
+    if(!token) return res.status(404).send({auth: false, message: 'El token no se formo correctamente.'})
 
     jwt.verify(token, config.secretKey, (err, decoded) => {
-        if(err) return res.status(500).send({auth: false, message: 'Failed to authorization token'})
+        if(err) return res.status(500).send({auth: false, message: 'Fallo al autorizar el codigo.'})
 
         req.userId = decoded.id
         // console.log(next)
